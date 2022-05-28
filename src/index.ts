81fab7a078e65d929fc5e1a6cf4a47f75d6ceff7
galkatz373 <galkatz373@gmail.com>
2022-05-29 00:04:25 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -122,7 +122,7 @@ const sendEmail = async (data: Scrap[]) => {
     const popularityNode = await row.$('.titleColumn .velocity');
     const popularity = await popularityNode.textContent();
 
-    if (imdbRatingNumber > 7.6) {
+    if (imdbRatingNumber > 9.2) {
       dataArr.push({
         popularity,
         title,
